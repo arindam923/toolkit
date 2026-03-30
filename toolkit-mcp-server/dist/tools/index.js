f0d35ddc427e7ffac0ab6283212e6a1d56de0ceb
arindam roy <arindam92@proton.me>
2026-03-31 01:31:49 +0530

    Remove toolkit-mcp-server from project (separate project)

--- toolkit-mcp-server/dist/tools/index.js
+++
@@ -1,7 +0,0 @@
-export { ImageResizeSchema, imageResize } from "./image-resize.js";
-export { ImageCompressSchema, imageCompress } from "./image-compress.js";
-export { ImageConvertSchema, imageConvert } from "./image-convert.js";
-export { ImageCropSchema, imageCrop } from "./image-crop.js";
-export { ImageRotateSchema, imageRotate } from "./image-rotate.js";
-export { ImageFilterSchema, imageFilter } from "./image-filter.js";
-//# sourceMappingURL=index.js.map
